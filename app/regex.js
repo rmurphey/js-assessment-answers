fcaebe86615b0bfb403bdadce6450962e4087e89
Rebecca Murphey <rmurphey@gmail.com>
2012-08-20 22:00:15 -0400

    adding regex answers

---
+++ app/regex.js
@@ -0,0 +1,29 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define(function() {
+  return {
+    containsNumber : function(str) {
+      return !!str.match(/\d/);
+    },
+
+    containsRepeatingLetter : function(str) {
+      return !!str.match(/([A-Za-z])\1/);
+    },
+
+    endsWithVowel : function(str) {
+      return !!str.match(/[aeiou]$/);
+    },
+
+    captureThreeNumbers : function(str) {
+      var matches = str.match(/\d{3}/);
+      if (matches) {
+        return matches[0];
+      }
+      return false;
+    },
+
+    matchesPattern : function(str) {
+      return !!str.match(/^\d{3}-\d{3}-\d{4}$/);
+    }
+  };
+});
