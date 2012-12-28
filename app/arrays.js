4c450a63af3b660f9ca6a53f82261fcc716c2153
Rebecca Murphey <rmurphey@gmail.com>
2012-12-28 09:24:03 -0500

    answer for array removeWithoutCopy
    fixes #9

--- app/arrays.js
+++ app/arrays.js
@@ -38,6 +38,19 @@ define(function() {
       return ret;
     },
 
+    removeWithoutCopy : function(arr, item) {
+      var i, len;
+
+      for (i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] === item) {
+          arr.splice(i, 1);
+          i = i - 1;
+        }
+      }
+
+      return arr;
+    },
+
     append : function(arr, item) {
       arr.push(item);
       return arr;
