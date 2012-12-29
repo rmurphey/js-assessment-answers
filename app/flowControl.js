da01a7f5064bf8cc902cf1bc22a7648645d4c2a5
Rebecca Murphey <rmurphey@gmail.com>
2012-12-29 01:01:17 -0500

    removing bad guard statement
    re #8 -- thanks @craveytrain!

--- app/flowControl.js
+++ app/flowControl.js
@@ -6,13 +6,11 @@ define(function() {
       // write a function that receives a number as its argument;
       // if the number is divisible by 3, the function should return 'fizz';
       // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible the 3 and 5, the function should return
+      // if the number is divisible by 3 and 5, the function should return
       // 'fizzbuzz';
       //
       // otherwise the function should return the number, or false if no number
       // was provided or if the value provided was not a number
-
-      if (!num) { return false; }
       if (typeof num !== 'number') { return false; }
 
       // not divisible by 3 or 5
