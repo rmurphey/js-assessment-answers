0427aeb1b712a214ee4baa8ea0d441bde1ea2e2b
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2013-11-29 09:52:25 +0100

    A more elegant and concise solution for the permute test

--- app/recursion.js
+++ app/recursion.js
@@ -32,48 +32,20 @@ define(function() {
     },
 
     permute: function(arr) {
-      // http://stackoverflow.com/a/11509565/54468
-      var temp = [];
-      var answer = [];
-
-      return doIt(arr);
-
-      function doIt(a) {
-        var i, len, item;
-
-        for (i = 0, len = arr.length; i < len; i++) {
-          // remove the item at index i
-          item = arr.splice(i, 1)[0];
-
-          // add that item to the array we're building up
-          temp.push(item);
-
-          if (arr.length) {
-            // if there's still anything left in the array,
-            // recurse over what's left
-            doIt(arr);
-          } else {
-            // otherwise, log the result and move on
-            logResult();
+      function permute(arr, pre, results) {
+        if (arr.length < 2) {
+          results.push(pre.concat(arr));          
+        } else {
+          for (var i = 0; i < arr.length; i++) {
+            var clone = arr.slice(0);
+            var item = clone.splice(i, 1);
+            permute(clone, pre.concat(item), results);
           }
-
-          // restore the item we removed at index i
-          // and remove it from our temp array
-          arr.splice(i, 0, item);
-          temp.pop();
         }
+        return results;
 
-        return answer;
       }
-
-      function logResult() {
-        answer.push(
-          // make a copy of temp using .slice()
-          // so we can continue to work with temp
-          temp.slice()
-        );
-      }
+      return permute(arr, [], []);
     }
   };
 });
-
