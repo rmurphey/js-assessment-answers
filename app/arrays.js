1c88f37ea0c9e485aee4560b0995397309fe2a54
Matt Surabian <matt.surabian@ceros.com>
2013-12-04 20:42:05 -0500

    Faster duplicate finder

--- app/arrays.js
+++ app/arrays.js
@@ -93,22 +93,18 @@ define(function() {
       return count;
     },
 
-    duplicates : function(arr) {
-      var seen = {};
-      var dupes = [];
+      duplicates : function(arr) {
+          var dupes = [];
 
-      for (var i = 0, len = arr.length; i < len; i++) {
-        seen[arr[i]] = seen[arr[i]] ? seen[arr[i]] + 1 : 1;
-      }
+          for (var i = 0, len = arr.length; i < len; i++) {
+              var val = arr[i];
+              if(arr.indexOf(val) !== arr.lastIndexOf(val) && dupes.indexOf(val) === -1){
+                  dupes.push(val);
+              }
+          }
 
-      for (var item in seen) {
-        if (seen.hasOwnProperty(item) && seen[item] > 1) {
-          dupes.push(item);
-        }
-      }
-
-      return dupes;
-    },
+          return dupes;
+      },
 
     square : function(arr) {
       var ret = [];
