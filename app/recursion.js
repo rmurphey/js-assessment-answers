0b894cb224966fbcb1c5cf031d165eb5ba35c9a9
MJGz <MJGz@users.noreply.github.com>
2016-02-29 01:32:06 -0800

    Update recursion.js
    Added alternate code for listFiles(). Added code for fibonacci() and validParentheses().

--- app/recursion.js
+++ app/recursion.js
@@ -1,7 +1,27 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers  = {
+  
   listFiles: function(data, dirName) {
+    
+    /*
+   // thisdir: does the dir of the data contribute to the answer?
+    var thisdir = (dir === undefined) || (dir === null) || (data.dir === dir),
+      rslt = [],
+      elval;
+    for ( var el in data.files) {
+      // elements in 'files' (elval) can be filename (a string) or object
+      elval = data.files[el];
+      if (typeof elval === 'string') {
+        if (thisdir) { rslt.push(elval); }
+      } else {
+        // recursion
+        rslt = rslt.concat(this.listFiles(elval, thisdir ? elval.dir : dir));
+      }
+    }
+    return rslt;
+    */
+    
     var listOfFiles = [];
     var dirs = [];
 
@@ -72,5 +92,41 @@ exports.recursionAnswers  = {
         temp.slice()
       );
     }
+  },
+  
+  fibonacci: function (n) {
+    // a is the previous-previous number in the series
+    // b is the previous number (latest) in the series
+    // q is which number in the series to return
+    var fib = function (a, b, q) {
+      if (q === 0) { return a; }
+      if (q === 1) { return b; }
+      // recursion
+      return fib(b, a + b, q - 1);
+    };
+    return fib(0, 1, n);
+  },
+  
+  validParentheses: function (n) {
+    var vParens = function (nInPar, nOutPar) {
+      var result = [];
+      var theRest = [];
+      // terminal case
+      if (nOutPar === 0) { return ['']; }
+      if (nInPar > 0) {
+        // can output '('
+        // recursion
+        theRest = vParens(nInPar - 1, nOutPar);
+        theRest.forEach((el) => result.push('('.concat(el)));
+      }
+      if (nOutPar > nInPar) {
+        // can output ')'
+        // recursion
+        theRest = vParens(nInPar, nOutPar - 1);
+        theRest.forEach((el) => result.push(')'.concat(el)));
+      }
+      return result;
+    };
+    return vParens(n, n);
   }
 };
